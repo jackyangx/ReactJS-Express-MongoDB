cf0e393908689786a78b1bb538e853a75909ba17
LZH 996633 <zhihui.lei@students.plymouth.ac.uk>
2021-01-13 10:49:38 +0800

    updated the information in about and added function descriptions

--- client/src/pages/page.about.js
+++ client/src/pages/page.about.js
@@ -21,7 +21,30 @@ class About extends BasePage {
   }
 
   render() {
-    return <div className={styles.defaultCss}>About</div>;
+    return (
+      <div className={styles.defaultCss}>
+        <div className="row">
+          <div className="col">
+            <div>1) User login</div>
+            <div>2) User registration</div>
+            <div>3) User log out</div>
+            <div>4) User search items</div>
+            <div>5) Users view projects they create</div>
+            <div>6) Users view their projects</div>
+            <div>7) Users create, modify and delete their own projects</div>
+            <div>8) Project includes name, time, period, participants, project status and project description</div>
+            <div>9) Users view tasks they create</div>
+            <div>10) Users view the tasks they participate in</div>
+            <div>11) Project sponsor create task</div>
+            <div>12) Tasks include sequence number, name, person, time and status</div>
+            <div>13) User modify task status</div>
+            <div>14) Project sponsor modify and delete task</div>
+            <div>15) Users view project data statistics</div>
+            <div>16) Online communication of project team members</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
